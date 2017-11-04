91d887c78671b20be87c3cf0f4e8733797d3cdc5
GuyLou <guylou@outlook.com>
2017-11-04 23:12:48 +0200

    Create activity.php

---
+++ oc_2.1.1.14.coffeeshopcrm.ocmod/upload/admin/language/english/coffeeshopcrm/activity.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']                = 'Customer Activity History';
+
+// Text
+$_['text_customer_address_add']    = '<a href="customer_id=%d">%s</a> added a new address.';
+$_['text_customer_address_edit']   = '<a href="customer_id=%d">%s</a> updated his/ her address.';
+$_['text_customer_address_delete'] = '<a href="customer_id=%d">%s</a> erased one of his/ her address(es).';
+$_['text_customer_edit']           = '<a href="customer_id=%d">%s</a> changed their account details.';
+$_['text_customer_forgotten']      = '<a href="customer_id=%d">%s</a> has requested a new password.';
+$_['text_customer_login']          = '<a href="customer_id=%d">%s</a> logged in.';
+$_['text_customer_password']       = '<a href="customer_id=%d">%s</a> changed his/ her password.';
+$_['text_customer_register']       = '<a href="customer_id=%d">%s</a> registered.';
+$_['text_customer_return_account'] = '<a href="customer_id=%d">%s</a> submitted a product <a href="return_id=%d">return</a>.';
+$_['text_customer_order_account']  = '<a href="customer_id=%d">%s</a> added a <a href="order_id=%d">new order</a>.';
+$_['text_account_update'] = '<a href="customer_id=%d">%s</a> has updated his/ her account.';
+
+
+
+// Text additions
+$_['text_customer_return_guest'] = '%s submitted a product <a href="return_id=%d">return</a>.';
+$_['text_customer_order_guest'] = '%s created a <a href="order_id=%d">new order</a>.';
+$_['text_affiliate_edit'] = '<a href="affiliate_id=%d">%s</a> updated their account details.';
+$_['text_affiliate_forgotten'] = '<a href="affiliate_id=%d">%s</a> requested a new password.';
+$_['text_affiliate_login'] = '<a href="affiliate_id=%d">%s</a> logged in.';
+$_['text_affiliate_password'] = '<a href="affiliate_id=%d">%s</a> updated their account password.';
+$_['text_affiliate_payment'] = '<a href="affiliate_id=%d">%s</a> updated their payment details.';
+$_['text_affiliate_register'] = '<a href="affiliate_id=%d">%s</a> registered for a new account.';
+
+
+
